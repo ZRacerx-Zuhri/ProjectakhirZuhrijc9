7340c37a858fdb906dba2b5e5438b24b1f17d93b
ZRacerx-Zuhri <zsuryaputra89@gmail.com>
2019-11-19 18:02:31 +0700

    update lg

---
+++ build/precache-manifest.562ed7ef286a0d508b5ecd11c1372697.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63b59470dba37699cfbe7d5d2786c88e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e74a9891c651ef69c9d",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1dc263dcff6a8bcc7dd6",
+    "url": "/static/css/main.ab66b708.chunk.css"
+  },
+  {
+    "revision": "8e74a9891c651ef69c9d",
+    "url": "/static/js/2.2f8b6b4d.chunk.js"
+  },
+  {
+    "revision": "1dc263dcff6a8bcc7dd6",
+    "url": "/static/js/main.8faf39c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "53011b271f2d70954ef2db84e559aa5f",
+    "url": "/static/media/avatar.53011b27.png"
+  },
+  {
+    "revision": "b9da230bcd176c5d43942d1f3e7e39ff",
+    "url": "/static/media/background-01.b9da230b.jpg"
+  },
+  {
+    "revision": "627264c30ecd25b76572586a03ac3746",
+    "url": "/static/media/background-02.627264c3.jpg"
+  },
+  {
+    "revision": "bf532bb8ed9573ec5000236b6297a69f",
+    "url": "/static/media/background-03.bf532bb8.jpeg"
+  },
+  {
+    "revision": "0817578c628525ce9baba6c9f977d55c",
+    "url": "/static/media/forbar.0817578c.jpg"
+  },
+  {
+    "revision": "da0f0777e5b89de583196e1316016e08",
+    "url": "/static/media/home3.da0f0777.jpg"
+  },
+  {
+    "revision": "2253b1707acd659c579d8a9a4bea4c02",
+    "url": "/static/media/noimage.2253b170.png"
+  },
+  {
+    "revision": "35b6c6d2b2dd6aa0dcf6d1128fb244e6",
+    "url": "/static/media/sport.35b6c6d2.jpg"
+  },
+  {
+    "revision": "f1e84893f9ea13cb07252de45df8531f",
+    "url": "/static/media/sport2.f1e84893.jpg"
+  }
+]);
